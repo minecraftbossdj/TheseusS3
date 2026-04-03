c51a8aff5a74f98e4e820e97ab29035c32c65508
riley <minecraftbossdj@gmail.com>
2026-04-03 13:50:00 -0400

    Revert "added kubejs"
    This reverts commit 453af36fea8e0c172f13eb32e865419f9b7936f9.

--- kubejs/startup_scripts/OHHH IM NETHERLESSING IT part 2 because idk if i can put these in the same file i probably can ngl also are you fr reading ts.js
+++
@@ -1,7 +0,0 @@
-StartupEvents.registry('fluid', event => { 
- event.create('ghast_tear')
-   .displayName('Liquid Ghast Tear')
-   .stillTexture('netherless:block/ghasttearfluid')
-   .flowingTexture('netherless:block/ghasttearfluid')
-   .bucketColor(0xb2d3ff)
-})
